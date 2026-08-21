fafecf7a86d15f537097b857552cb6eeade4c87c
stevelorellano896-wq <stevelorellano896@gmail.com>
2026-08-21 14:44:31 -0500

    Add files via upload

---
+++ ImprimirCabecera.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kof
+{
+    internal class Program
+    {
+        static void ImprimirCabecera(string nombreMateria, int grupo, string NombreEstudiante)
+        {
+            Console.WriteLine("=================================");
+            Console.WriteLine("  Universidad Del Caribe      ");
+            Console.WriteLine("  Stevel Orellano");
+            Console.WriteLine($"  Asignatura: {nombreMateria}");
+            Console.WriteLine($"  Grupo: {grupo}");
+            Console.WriteLine("================================");
+        }
+
+        public static void Main(string[] args)
+        {
+            ImprimirCabecera("Fundamentos de Programacion", 1, "Stevel Orellano");
+        }
+    }
+}
+
+
