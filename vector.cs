e47b2559a3499cf77e7015cf4cbfd50a997f6f89
stevelorellano896-wq <stevelorellano896@gmail.com>
2026-09-18 13:13:36 -0500

    Add initial vector program with array example

---
+++ vector.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vector
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numero = new int[5] { 12, 31, 32, -3, 3};
+            Console.WriteLine($"elemento en [0]: {numero[0]}");
+        }
+    }
+}
